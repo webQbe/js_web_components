4e434c230490d103b19ee8f9ac0e9f54ea064729
webQbe <webcubesolutionslk@gmail.com>
2024-10-28 19:45:13 +0530

    Created Shadow DOM & defined its template to apply separate style to UserCard elements

--- userCard.js
+++ userCard.js
@@ -1,13 +1,45 @@
 /* Creating Custom <user-card> Element */
 
+
+// Define Template Element for Shadow DOM
+const template = document.createElement('template');
+
+// Add Content to Template
+template.innerHTML = `
+
+    <style>
+        h3 {
+
+             color: coral;
+        
+        }
+    </style>
+    <div class="user-card">
+        <h3></h3>
+    </div>
+`;
+
+
+
 // Create UserCard class
 class UserCard extends HTMLElement {
 
     constructor(){
 
         super(); 
-        this.innerHTML = `<style> h3 { color: coral; }</style><h3>${this.getAttribute('name')}</h3>`; 
 
+        // Creating Shadow DOM
+        this.attachShadow({ mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // Select Element from ShadowRoot &
+        // Set h3 text to name attribute value
+        this.shadowRoot.querySelector('h3').innerText = 
+            this.getAttribute('name');
+
+
+        /*  this.innerHTML = `<style> h3 { color: coral; }</style><h3>${this.getAttribute('name')}</h3>`; 
+        */
         // super() calls constructor of HTMLElement class
         // 'this' pertains to custom element
         // Set Tag's innerHTML text to value of name attribute
